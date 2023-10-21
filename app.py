6704046c7249b034ebfae2db9f7129401331c459
enriq <enriquejiwa@gmail.com>
2023-10-21 18:11:11 +0200

    [8]feat: /login endpoint

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, request, jsonify
-from sqlalchemy.orm import Session
-from database import engine
-from models import User, Base
-from werkzeug.security import generate_password_hash
-
 import re
 
+from flask import Flask, jsonify, request
+from sqlalchemy.orm import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from database import engine
+from models import Base, User
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('POSTGRES_URL')
-# db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!' 
+    return 'Hello World!'
+
 
 @app.route('/init_db')
 def init_db():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     return "DB initialized"
+
 
 @app.post('/user')
 def create_user():
@@ -40,13 +41,14 @@ def create_user():
     # check if password is valid
     password_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$'
     if not re.match(password_pattern, password):
-        return jsonify({'mensaje': 'Contraseña no válida. La contraseña debe tener ' \
-                                   'como mínimo 6 caracteres, entre los cuales debe ' \
+        return jsonify({'mensaje': 'Contraseña no válida. La contraseña debe tener '
+                                   'como mínimo 6 caracteres, entre los cuales debe '
                                    'haber almenos una letra, un número y una mayúscula'}), 400
 
     with Session(engine) as session:
         # Check if the user with the same username or email already exists
-        existing_user = session.query(User).filter_by(username=username).first()
+        existing_user = session.query(
+            User).filter_by(username=username).first()
         if existing_user:
             return jsonify({'mensaje': 'Nombre de usuario ya existente'}), 400
 
@@ -55,16 +57,27 @@ def create_user():
             return jsonify({'mensaje': 'Dirección email ya existente'}), 400
 
         # Create a new user
-        new_user = User(username=username, email=email, password=generate_password_hash(password))
+        new_user = User(username=username, email=email,
+                        password=generate_password_hash(password))
         session.add(new_user)
         session.commit()
 
         return jsonify({'mensaje': 'Usuario '+username+' registrado correctamente'}), 201
 
-    
     return jsonify({'mensaje': 'Error'}), 400
-    
- 
+
+
+@app.post('/login')
+def login_user():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+
+    with Session(engine) as session:
+        user = session.query(User).filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            return jsonify({'success': False, 'error': 'Login details are incorrect'}), 401
+        return jsonify({'success': True}), 200
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-from dotenv import load_dotenv
-
 # Load environment variables from the .env file
-load_dotenv(dotenv_path='env/.env')
+load_dotenv(dotenv_path='.env')
 
 engine = create_engine(os.getenv('POSTGRES_URL'))
