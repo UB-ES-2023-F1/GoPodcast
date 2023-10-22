8c7861197a0bd131d14529d18fa60fc8da1d9d7d
enriq <enriquejiwa@gmail.com>
2023-10-22 17:59:55 +0200

    [10]feat: Refresh token on authed request

--- app.py
+++ app.py
@@ -20,6 +20,20 @@ app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(app)
 
 
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
