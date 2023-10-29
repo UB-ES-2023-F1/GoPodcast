c2d04bf917432b4e0a8b386ba278ea26ccac6b06
enriq <enriquejiwa@gmail.com>
2023-10-29 19:17:00 +0100

    [19]feat: Added tests

---
+++ tests/test_podcast.py
@@ -0,0 +1,53 @@
+import pytest
+from werkzeug.security import generate_password_hash
+
+from app import create_app
+from models import Podcast, User, db
+
+
+@pytest.fixture
+def app():
+    app = create_app(testing=True)
+    with app.app_context():
+        db.create_all()
+    yield app
+    with app.app_context():
+        db.drop_all()
+
+
+def test_post_episode(app):
+    with app.app_context():
+        user = User(
+            email="test@example.com",
+            username="test",
+            password=generate_password_hash("Test1234"),
+            verified=True
+        )
+        db.session.add(user)
+        db.session.commit()
+        podcast = Podcast(
+            cover=b"",
+            name="podcast",
+            description="description",
+            id_author=user.id
+        )
+        db.session.add(podcast)
+        db.session.commit()
+        id_podcast = podcast.id
+    data = {
+        "title": "title",
+        "description": "description",
+        "audio": (b"", "test.mp3", "audio/mpeg")
+    }
+    client = app.test_client()
+
+    # Unauthenticated
+    response = client.post(f"/podcasts/{id_podcast}/episodes", data=data)
+    assert response.status_code == 401
+
+    # Authenticated
+    response = client.post('/login', json={"email": "test@example.com",
+                                           "password": "Test1234"})
+    assert response.status_code == 200
+    response = client.post(f"/podcasts/{id_podcast}/episodes", data=data)
+    assert response.status_code == 201
